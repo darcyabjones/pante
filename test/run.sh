925cca2607e7b59445d805ab547cd1a2a304646a
Darcy Jones <darcy.ab.jones@gmail.com>
2019-10-18 02:11:11 +0800

    modified test command

--- test/run.sh
+++ test/run.sh
@@ -4,9 +4,7 @@ NXF_ANSI_LOG=false nextflow run ./main.nf \
   --genomes "test/*.fasta" \
   --outdir "test/results"
 
-# --rnammer \
-
-#  --repbase containers/downloads/RepBaseRepeatMaskerEdition-20181026.tar.gz \
-
-#  --rm_meta containers/downloads/RepeatMaskerMetaData-20181026.tar.gz \
-
+# --repbase downloads/RepBaseRepeatMaskerEdition-20181026.tar.gz
+#  --rm_meta downloads/RepeatMaskerMetaData-20181026.tar.gz
+# --species "Ascomycota"
+# --rnammer
