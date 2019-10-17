05ed6ae7906cc851886d9208dbc9a321197b24e7
Darcy Jones <darcy.ab.jones@gmail.com>
2019-10-17 10:55:29 +0800

    Fixed error in config for containers.

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 NXF_ANSI_LOG=false nextflow run ./main.nf \
-  -profile singularity,standard \
+  -profile singularity_plus,standard \
   -resume \
   --genomes "test/*.fasta" \
   --repbase containers/downloads/RepBaseRepeatMaskerEdition-20181026.tar.gz \
@@ -8,7 +8,3 @@ NXF_ANSI_LOG=false nextflow run ./main.nf \
   --noinfernal \
   --rnammer \
   --outdir "test/results"
-
-
-
-
